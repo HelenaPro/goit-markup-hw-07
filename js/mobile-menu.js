78c462f9517de524761fac8d28be5706ed3bc441
HelenaPro <eleonnorrap@gmail.com>
2023-04-16 03:14:18 +0300

    summary (required)

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,15 +1,14 @@
 (() => {
     const refs = {
-        openMenuBtn: document.querySelector("[data-menu-open]"),
-        closeMenuBtn: document.querySelector("[data-menu-close]"),
-        menu: document.querySelector("[data-menu]"),
-      };
-    
-      refs.openMenuBtn.addEventListener("click", toggleMenu);
-      refs.closeMenuBtn.addEventListener("click", toggleMenu);
-    
-      function toggleMenu() {
-        refs.menu.classList.toggle("is-hidden");
-        document.body.classList.toggle("no-scroll");
+      openModalBtn: document.querySelector("[data-mobil-open]"),
+      closeModalBtn: document.querySelector("[data-mobil-close]"),
+      modal: document.querySelector("[data-mobil]"),
+    };
+  
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-hidden");
     }
   })();
